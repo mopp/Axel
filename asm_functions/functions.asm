2e028f1e0e94a5afd8785b7ae1c17034c32656fe
mopp <ac.kxp165@gmail.com>
2013-09-20 10:46:10 +0900

    add asm files

---
+++ asm_functions/functions.asm
@@ -0,0 +1,111 @@
+;---------------------------------------------------------------------
+; vim:ft=nasm:foldmethod=marker
+; File: asm_functions/functions.asm
+; Description:
+;       It provides some assembly only functions to call those in C.
+;---------------------------------------------------------------------
+    bits 32
+
+    global io_hlt, io_cli, io_sti
+    global io_in8, io_in16, io_in32
+    global io_out8, io_out16, io_out32
+    global load_gdtr, load_idtr
+    global change_segment_selectors
+
+section .text
+
+
+; void io_hlt(void);
+io_hlt:
+    hlt
+    ret
+
+
+; void io_cli(void);
+io_cli:
+    cli
+    ret
+
+
+; void io_sti(void);
+io_sti:
+    sti
+    ret
+
+
+; int io_in8(int port);
+io_in8:
+    mov EDX, [ESP + 4] ; port
+    mov EAX, 0
+    in  AL, DX
+    ret
+
+
+; int io_in16(int port);
+io_in16:
+    mov EDX, [ESP + 4] ; port
+    mov EAX, 0
+    in  AX, DX
+    ret
+
+
+; int io_in32(int port);
+io_in32:
+    mov EDX, [ESP + 4] ; port
+    mov EAX, 0
+    in  EAX, DX
+    ret
+
+
+; void io_out8(int port, int data);
+io_out8:
+    mov EDX, [ESP + 4]  ; port
+    mov AL, [ESP + 8]   ; data
+    out DX, AL
+    ret
+
+
+; void io_out16(int port, int data);
+io_out16:
+    mov EDX, [ESP + 4]  ; port
+    mov EAX, [ESP + 8]  ; data
+    out DX, AX
+    ret
+
+
+; void io_out32(int port, int data);
+io_out32:
+    mov EDX, [ESP + 4]  ; port
+    mov EAX, [ESP + 8]  ; data
+    out DX, EAX
+    ret
+
+
+; void load_gdtr(int limit_byte, int addr);
+; 大きさが2byte アドレスが4byte 合わせて6byteをlgdtが読み込む
+; なので、limit_byteの下位2byteをずらしている。 ldtrも同様
+load_gdtr:
+    mov AX, [ESP + 4]      ; limit
+    mov [ESP + 6], AX
+    lgdt [ESP + 6]
+    ret
+
+
+; void load_idtr(int limit_byte, int addr);
+load_idtr:
+    mov AX, [ESP + 4]      ; limit
+    mov [ESP + 6], AX
+    lidt [ESP + 6]
+    ret
+
+; void change_segment_selectors(int data_segment);
+change_segment_selectors:
+    jmp 0x08:.jmp_point
+.jmp_point:
+    mov AX, [ESP + 4]
+    mov ES, AX
+    mov DS, AX
+    mov FS, AX
+    mov GS, AX
+    mov SS, AX
+    ret
---
+++ boot/multiboot.asm
@@ -0,0 +1,58 @@
+;---------------------------------------------------------------------
+; vim:ft=nasm:foldmethod=marker
+; File: boot/multiboot.asm
+; Description: It is called by grub and call kernel entry point.
+;---------------------------------------------------------------------
+    bits 32
+
+#include <multiboot_constants.h>
+
+; 上記定数を用いてヘッダを書き込み
+section .multiboot_header
+align 4
+    dd MULTIBOOT_HEADER_MAGIC
+    dd (MULTIBOOT_PAGE_ALIGN_BIT | MULTIBOOT_MEMORY_INFO_BIT | MULTIBOOT_VIDEO_MODE_BIT)
+    dd -(MULTIBOOT_HEADER_MAGIC + (MULTIBOOT_PAGE_ALIGN_BIT | MULTIBOOT_MEMORY_INFO_BIT | MULTIBOOT_VIDEO_MODE_BIT))
+
+    ; Address field
+    dd $0
+    dd $0
+    dd $0
+    dd $0
+    dd $0
+
+    ; Graphic field
+    ; dd $0
+    dd 1
+    dd DISPLAY_MAX_X
+    dd DISPLAY_MAX_Y
+    dd DISPLAY_MAX_BIT
+
+
+section .kernel_init_stack
+stack_bottom:
+    times 16384 db 0
+stack_top:
+
+
+section .text
+    extern kernel_entry
+
+    ; リンク時にboot_kernelをエントリポイントとして指定すること
+    global boot_kernel
+boot_kernel:
+    cli
+
+    cmp EAX, MULTIBOOT_BOOTLOADER_MAGIC
+    jne .sleep
+
+    mov esp,  stack_top
+
+    ; ブート情報構造体の格納アドレスを引数へ
+    push EBX
+    call kernel_entry
+
+    sti
+.sleep:
+    hlt
+    jmp .sleep
