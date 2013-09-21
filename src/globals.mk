9d1ee7da7d7394fcb9ca71cfe7fd59ac82959b5b
mopp <mopp@mopp>
2013-09-22 01:07:12 +0900

    fix makefiles

--- src/globals.mk
+++ src/globals.mk
@@ -1,12 +1,12 @@
 # := で 代入時に展開
-MAKEFILE 			:= Makefile
-AXEL_INCLUDE_PATH 	:= $(AXEL_ROOT_DIR)include/
+
+# Path
 TOOL_PATH 			:= /home/mopp/.mopp/
 
+# command
 RM 					:= rm -rf
-GCC					:= i686-elf-gcc -I$(AXEL_INCLUDE_PATH) -I$(TOOL_PATH)include/ -L$(TOOL_PATH)lib/
+GCC					:= i686-elf-gcc
 NASM 				:= nasm -f elf32
-LIB_ASM				:= libasmf.a
 AR					:= i686-elf-ar
 RANLIB				:= i686-elf-ranlib
 QEMU_EXE 			:= qemu-system-x86_64 -monitor stdio -vga std
