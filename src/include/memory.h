94249394e6e439986f52aff8b8f0d80efe06e9d5
mopp <mopp@mopp>
2013-12-03 17:33:41 +0900

    add memory functions

--- src/include/memory.h
+++ src/include/memory.h
@@ -6,6 +6,8 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#include <stdint.h>
+
 /* page_table_entry -> page_table -> page_directory_entry -> page_directory_table */
 
 /* ページ一つに相当し、ページについての情報を保持する */
@@ -58,10 +60,26 @@ typedef struct page_directory_entry Page_directory_entry;
 _Static_assert(sizeof(Page_directory_entry) == 4, "Static ERROR : Page_directory_entry size is NOT 4 byte(32 bit).");
 
 enum paging_constants {
+    PAGE_SIZE       = 4096,
     PAGE_TABLE_SIZE = 1024,
 };
 
 /* the page_table size is 1024(PAGE_TABLE_SIZE) * 4KB(1page) = 4MB */
 /* extern Page_table_entry page_table[PAGE_TABLE_SIZE]; */
+
+
+/* リンカスクリプトで設定される数値を取得 */
+uint32_t get_kernel_start_addr(void);
+uint32_t get_kernel_end_addr(void);
+uint32_t get_kernel_size(void);
+
+/* メモリ管理を行う */
+struct memory_manager {
+    uint32_t frees, lost_size, lost_times;
+    struct  {
+        uint32_t base_addr, size;
+    } mem_info[1024];
+};
+typedef struct memory_manager Memory_manager;
 
 #endif
--- src/memory.c
+++ src/memory.c
@@ -1,1 +1,46 @@
+/************************************************************
+ * File: graphic_txt.c
+ * Description: some memory functions.
+ ************************************************************/
+
 #include <memory.h>
+#include <stdint.h>
+#include <stddef.h>
+
+
+/* &をよく忘れるので関数で包む */
+extern uint32_t LD_KERNEL_SIZE;
+extern uint32_t LD_KERNEL_START;
+extern uint32_t LD_KERNEL_END;
+
+
+uint32_t get_kernel_start_addr(void) {
+    return (uint32_t)&LD_KERNEL_START;
+}
+
+
+uint32_t get_kernel_end_addr(void) {
+    return (uint32_t)&LD_KERNEL_END;
+}
+
+
+uint32_t get_kernel_size(void) {
+    return (uint32_t)&LD_KERNEL_SIZE;
+}
+
+Memory_manager* get_inited_memory_manager(void) {
+    Memory_manager* m = (Memory_manager*)get_kernel_end_addr();
+
+    return m;
+}
+
+void* memset(void* buf, int ch, size_t len) {
+    char* end = buf + len;
+    char* t = buf;
+
+    while (t <= end) {
+        *t++ = ch;
+    }
+
+    return buf;
+}
