8f5caefb0d7e84c7113540203f3bdbee1919c009
mopp <ac.kxp165@gmail.com>
2014-02-24 01:03:56 +0900

    add graphic and state code files

---
+++ src/graphic.c
@@ -0,0 +1,15 @@
+/************************************************************
+ * File: graphic.c
+ * Description: some output functions for Normal Graphic.
+ ************************************************************/
+
+#include <graphic.h>
+#include <vbe.h>
+#include <state_code.h>
+
+static char* vram;
+
+Axel_state_code init_graphic(Vbe_info_block const * const info, Vbe_mode_info_block const * const m_info) {
+
+    return SUCCESS;
+}
---
+++ src/include/state_code.h
@@ -0,0 +1,10 @@
+/************************************************************
+ * File: include/state_code.h
+ * Description: There are Axel StateCode.
+ ************************************************************/
+
+enum Axel_state_code {
+    SUCCESS,
+    FAILED,
+};
+typedef enum Axel_state_code Axel_state_code;
