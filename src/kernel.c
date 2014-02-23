588e43a1627722b78e97a5f06b3f899e6ac5bf5a
mopp <ac.kxp165@gmail.com>
2014-02-24 01:04:38 +0900

    wip for graphic(VBE2.0)

--- src/kernel.c
+++ src/kernel.c
@@ -7,6 +7,7 @@
 #include <kernel.h>
 #include <multiboot_constants.h>
 #include <multiboot_structs.h>
+// TODO: GRAPHIC_MODE か TEXT_MODE で切り替える
 #include <graphic_txt.h>
 #include <interrupt_handler.h>
 #include <stddef.h>
@@ -83,177 +84,166 @@ _Noreturn void kernel_entry(Multiboot_info* boot_info) {
 
         if (vbe_mode_info->phys_base_ptr != 0) {
             int const byte_per_pixel = (vbe_mode_info->bits_per_pixel / 8);
-            int const size = vbe_mode_info->x_resolution * vbe_mode_info->y_resolution * byte_per_pixel;
+            int const size = (vbe_mode_info->x_resolution) * vbe_mode_info->y_resolution * byte_per_pixel / sizeof(uint32_t);
 
-            char* const vram = (char*)vbe_mode_info->phys_base_ptr;
+            uint32_t* const vram = (uint32_t*)vbe_mode_info->phys_base_ptr;
 
-            int const rs = vbe_mode_info->red_mask_size;
-            int const gs = vbe_mode_info->green_mask_size;
-            int const bs = vbe_mode_info->blue_mask_size;
+            uint32_t const rs = vbe_mode_info->red_mask_size;
+            uint32_t const gs = vbe_mode_info->green_mask_size;
+            uint32_t const bs = vbe_mode_info->blue_mask_size;
+            uint32_t const rsvs = vbe_mode_info->rsvd_mask_size;
+            // all 8bit
 
-            int const rp = vbe_mode_info->red_field_position;
-            int const gp = vbe_mode_info->green_field_position;
-            int const bp = vbe_mode_info->blue_field_position;
+            uint32_t const rp = vbe_mode_info->red_field_position;
+            uint32_t const gp = vbe_mode_info->green_field_position;
+            uint32_t const bp = vbe_mode_info->blue_field_position;
+            uint32_t const rsvp = vbe_mode_info->rsvd_field_position;
 
-            /* uint32_t rgb_to_direct_color(uint8_t r, uint8_t g, uint8_t b) { */
-            /* for (int i = 0; i < count; i++) { */
-            /* vram[]; */
-            /* } */
-            /* } */
-
-            for (int i = 0; i < size; i += byte_per_pixel) {
-                /* if ((rs + gs + bs) == vbe_mode_info->bits_per_pixel) { */
-                if ((rs + gs + bs + vbe_mode_info->rsvd_mask_size) == vbe_mode_info->bits_per_pixel) {
-                    vram[i] = (0 << rp) + (0 << gp) + (31 << bp);
+            for (int i = 0; i < size; i++) {
+                if ((rs + gs + bs + rsvs) == 32) {
+                    vram[i] = (0xff << rp) + (0x00 << gp) + (0xff << bp) + (0 << rsvp);
                 } else {
-                    vram[i] = (31 << rp) + (63 << gp) + (31 << gp);
+                    vram[i] = (0 << rp) + (0 << gp) + (0 << gp);
                 }
-
-                /*
-                 * if (6 == vbe_mode_info->memory_model_type) {
-                 *     [> Direct Color <]
-                 *     vram[i] = (i % 2) ? 0x55 : 0xAA;
-                 * } else {
-                 *     vram[i] = 0xff;
-                 * }
-                 */
-            }
             }
         }
-
-        for(;;) {
-            puts("\n-------------------- hlt ! --------------------\n");
-            io_hlt();
-        }
+    } else {
+        puts("\nMULTIBOOT_INFO_HAS_VIDEO_INFO is disable !\n");
     }
 
+    for(;;) {
+        puts("\n-------------------- hlt ! --------------------\n");
+        io_hlt();
+    }
+}
 
-    static Segment_descriptor* set_segment_descriptor(Segment_descriptor* s, uint32_t limit, uint32_t base_addr, uint8_t type, uint8_t type_flag, uint8_t pliv, uint8_t p_flag, uint8_t op_flag, uint8_t g_flag) {
-        /* セグメントのサイズを設定 */
-        /* granularity_flagが1のときlimit * 4KBがセグメントのサイズになる */
-        s->limit_low = (limit & 0x0000ffff);
-        s->limit_hi  = (limit >> 16);
 
-        /* セグメントの開始アドレスを設定 */
-        s->base_addr_low = (base_addr & 0x0000ffff);
-        s->base_addr_mid = (base_addr & 0x00ff0000) >> 16;
-        s->base_addr_hi  = (base_addr & 0xff000000) >> 24;
+static Segment_descriptor* set_segment_descriptor(Segment_descriptor* s, uint32_t limit, uint32_t base_addr, uint8_t type, uint8_t type_flag, uint8_t pliv, uint8_t p_flag, uint8_t op_flag, uint8_t g_flag) {
+    /* セグメントのサイズを設定 */
+    /* granularity_flagが1のときlimit * 4KBがセグメントのサイズになる */
+    s->limit_low = (limit & 0x0000ffff);
+    s->limit_hi  = (limit >> 16);
 
-        /* アクセス権エラー */
-        if (0xf < type) {
-            return NULL;
-        }
-        /* アクセス権を設定 */
-        s->type = type & 0x0f;
+    /* セグメントの開始アドレスを設定 */
+    s->base_addr_low = (base_addr & 0x0000ffff);
+    s->base_addr_mid = (base_addr & 0x00ff0000) >> 16;
+    s->base_addr_hi  = (base_addr & 0xff000000) >> 24;
 
-        /* セグメントタイプエラー */
-        if (0x1 < type_flag) {
-            return NULL;
-        }
-        /* 0でシステム用, 1でコードかデータ用のセグメントとなる */
-        s->segment_type = type_flag;
+    /* アクセス権エラー */
+    if (0xf < type) {
+        return NULL;
+    }
+    /* アクセス権を設定 */
+    s->type = type & 0x0f;
 
-        /* 特権レベルエラー */
-        if (0x3 < pliv) {
-            return NULL;
-        }
-        /* 特権レベルを設定 */
-        s->plivilege_level = pliv;
+    /* セグメントタイプエラー */
+    if (0x1 < type_flag) {
+        return NULL;
+    }
+    /* 0でシステム用, 1でコードかデータ用のセグメントとなる */
+    s->segment_type = type_flag;
 
-        /* メモリに存在する */
-        s->present_flag = p_flag;
-        /* OSが任意に使用できる */
-        s->available = 0;
-        /* 0で予約されている */
-        s->zero_reserved = 0;
-        /* 扱う単位を16bitか32bitか設定 */
-        s->default_op_size = IS_FLAG_NOT_ZERO(op_flag);
-        /* セグメントサイズを4KB単位とする */
-        s->granularity_flag = IS_FLAG_NOT_ZERO(g_flag);
+    /* 特権レベルエラー */
+    if (0x3 < pliv) {
+        return NULL;
+    }
+    /* 特権レベルを設定 */
+    s->plivilege_level = pliv;
 
-        return s;
+    /* メモリに存在する */
+    s->present_flag = p_flag;
+    /* OSが任意に使用できる */
+    s->available = 0;
+    /* 0で予約されている */
+    s->zero_reserved = 0;
+    /* 扱う単位を16bitか32bitか設定 */
+    s->default_op_size = IS_FLAG_NOT_ZERO(op_flag);
+    /* セグメントサイズを4KB単位とする */
+    s->granularity_flag = IS_FLAG_NOT_ZERO(g_flag);
+
+    return s;
+}
+
+
+static inline void init_gdt(void) {
+    Segment_descriptor *gdt = (Segment_descriptor*)GDT_ADDR;
+
+    /* 全セグメントを初期化 */
+    for (int i = 0; i < GDT_MAX_NUM; ++i) {
+        set_segment_descriptor(gdt + i, 0, 0, 0, 0, 0, 0, 0, 0);
     }
 
+    /* 全アドレス空間を指定 */
+    /* Flat Setup */
+    set_segment_descriptor(gdt + GDT_KERNEL_CODE_INDEX, 0xffffffff, 0x00000000, GDT_TYPE_CODE_EXR, GDT_TYPE_FOR_CODE_DATA, GDT_RING0, GDT_PRESENT, GDT_DB_OPSIZE_32BIT, GDT_GRANULARITY_4KB);
+    set_segment_descriptor(gdt + GDT_KERNEL_DATA_INDEX, 0xffffffff, 0x00000000, GDT_TYPE_DATA_RWA, GDT_TYPE_FOR_CODE_DATA, GDT_RING0, GDT_PRESENT, GDT_DB_OPSIZE_32BIT, GDT_GRANULARITY_4KB);
 
-    static inline void init_gdt(void) {
-        Segment_descriptor *gdt = (Segment_descriptor*)GDT_ADDR;
+    load_gdtr(GDT_LIMIT, GDT_ADDR);
+    change_segment_selectors(GDT_KERNEL_DATA_INDEX * 8);
+}
 
-        /* 全セグメントを初期化 */
-        for (int i = 0; i < GDT_MAX_NUM; ++i) {
-            set_segment_descriptor(gdt + i, 0, 0, 0, 0, 0, 0, 0, 0);
-        }
 
-        /* 全アドレス空間を指定 */
-        /* Flat Setup */
-        set_segment_descriptor(gdt + GDT_KERNEL_CODE_INDEX, 0xffffffff, 0x00000000, GDT_TYPE_CODE_EXR, GDT_TYPE_FOR_CODE_DATA, GDT_RING0, GDT_PRESENT, GDT_DB_OPSIZE_32BIT, GDT_GRANULARITY_4KB);
-        set_segment_descriptor(gdt + GDT_KERNEL_DATA_INDEX, 0xffffffff, 0x00000000, GDT_TYPE_DATA_RWA, GDT_TYPE_FOR_CODE_DATA, GDT_RING0, GDT_PRESENT, GDT_DB_OPSIZE_32BIT, GDT_GRANULARITY_4KB);
+static Gate_descriptor* set_gate_descriptor(Gate_descriptor* g, uint8_t gate_type, void(*offset)(void), uint16_t selector_index, uint8_t gate_size, uint8_t pliv, uint8_t p_flag) {
+    g->offset_low   = ((uint32_t)offset & 0x0000ffff);
+    g->offset_high  = ((uint32_t)offset >> 16);
 
-        load_gdtr(GDT_LIMIT, GDT_ADDR);
-        change_segment_selectors(GDT_KERNEL_DATA_INDEX * 8);
+    g->segment_selector = selector_index * GDT_ELEMENT_SIZE;
+
+    g->type = gate_type;
+    g->size = gate_size;
+
+    g->unused_zero = 0x00;
+    g->zero_reserved = 0x0;
+
+    /* 特権レベルを設定 */
+    g->plivilege_level = pliv;
+
+    g->present_flag = IS_FLAG_NOT_ZERO(p_flag);
+
+    return g;
+}
+
+
+static void init_idt(void) {
+    Gate_descriptor *idt = (Gate_descriptor*)IDT_ADDR;
+
+    /* 全ゲートディスクリプタ初期化を初期化 */
+    for (int i = 0; i < IDT_MAX_NUM; ++i) {
+        set_gate_descriptor(idt + i, 0, 0, 0, 0, 0, 0);
     }
+    load_idtr(IDT_LIMIT, IDT_ADDR);
+
+    set_gate_descriptor(idt + 0x20, IDT_GATE_TYPE_INTERRUPT, asm_interrupt_handler0x20, GDT_KERNEL_CODE_INDEX, IDT_GATE_SIZE_32, IDT_RING0, IDT_PRESENT);
+}
 
 
-    static Gate_descriptor* set_gate_descriptor(Gate_descriptor* g, uint8_t gate_type, void(*offset)(void), uint16_t selector_index, uint8_t gate_size, uint8_t pliv, uint8_t p_flag) {
-        g->offset_low   = ((uint32_t)offset & 0x0000ffff);
-        g->offset_high  = ((uint32_t)offset >> 16);
+static inline void init_pic(void) {
+    /* 全割り込みを受けない */
+    io_out8(PIC0_IMR_DATA_PORT, PIC_IMR_MASK_IRQ_ALL);
+    io_out8(PIC1_IMR_DATA_PORT, PIC_IMR_MASK_IRQ_ALL);
 
-        g->segment_selector = selector_index * GDT_ELEMENT_SIZE;
+    /* Master */
+    io_out8(PIC0_CMD_STATE_PORT, PIC0_ICW1);
+    io_out8(PIC0_IMR_DATA_PORT,  PIC0_ICW2);
+    io_out8(PIC0_IMR_DATA_PORT,  PIC0_ICW3);
+    io_out8(PIC0_IMR_DATA_PORT,  PIC0_ICW4);
 
-        g->type = gate_type;
-        g->size = gate_size;
+    /* Slave */
+    io_out8(PIC1_CMD_STATE_PORT, PIC1_ICW1);
+    io_out8(PIC1_IMR_DATA_PORT,  PIC1_ICW2);
+    io_out8(PIC1_IMR_DATA_PORT,  PIC1_ICW3);
+    io_out8(PIC1_IMR_DATA_PORT,  PIC1_ICW4);
 
-        g->unused_zero = 0x00;
-        g->zero_reserved = 0x0;
-
-        /* 特権レベルを設定 */
-        g->plivilege_level = pliv;
-
-        g->present_flag = IS_FLAG_NOT_ZERO(p_flag);
-
-        return g;
-    }
-
-
-    static void init_idt(void) {
-        Gate_descriptor *idt = (Gate_descriptor*)IDT_ADDR;
-
-        /* 全ゲートディスクリプタ初期化を初期化 */
-        for (int i = 0; i < IDT_MAX_NUM; ++i) {
-            set_gate_descriptor(idt + i, 0, 0, 0, 0, 0, 0);
-        }
-        load_idtr(IDT_LIMIT, IDT_ADDR);
-
-        set_gate_descriptor(idt + 0x20, IDT_GATE_TYPE_INTERRUPT, asm_interrupt_handler0x20, GDT_KERNEL_CODE_INDEX, IDT_GATE_SIZE_32, IDT_RING0, IDT_PRESENT);
-    }
+    /* IRQ2以外の割り込みを受け付けない */
+    io_out8(PIC0_IMR_DATA_PORT, PIC_IMR_MASK_IRQ_ALL & (~PIC_IMR_MASK_IRQ2));
+}
 
 
-    static inline void init_pic(void) {
-        /* 全割り込みを受けない */
-        io_out8(PIC0_IMR_DATA_PORT, PIC_IMR_MASK_IRQ_ALL);
-        io_out8(PIC1_IMR_DATA_PORT, PIC_IMR_MASK_IRQ_ALL);
+static inline void init_pit(void) {
+    io_out8(PIT_PORT_CONTROL, PIT_ICW);
+    io_out8(PIT_PORT_COUNTER0, PIT_COUNTER_VALUE_LOW);
+    io_out8(PIT_PORT_COUNTER0, PIT_COUNTER_VALUE_HIGH);
 
-        /* Master */
-        io_out8(PIC0_CMD_STATE_PORT, PIC0_ICW1);
-        io_out8(PIC0_IMR_DATA_PORT,  PIC0_ICW2);
-        io_out8(PIC0_IMR_DATA_PORT,  PIC0_ICW3);
-        io_out8(PIC0_IMR_DATA_PORT,  PIC0_ICW4);
-
-        /* Slave */
-        io_out8(PIC1_CMD_STATE_PORT, PIC1_ICW1);
-        io_out8(PIC1_IMR_DATA_PORT,  PIC1_ICW2);
-        io_out8(PIC1_IMR_DATA_PORT,  PIC1_ICW3);
-        io_out8(PIC1_IMR_DATA_PORT,  PIC1_ICW4);
-
-        /* IRQ2以外の割り込みを受け付けない */
-        io_out8(PIC0_IMR_DATA_PORT, PIC_IMR_MASK_IRQ_ALL & (~PIC_IMR_MASK_IRQ2));
-    }
-
-
-    static inline void init_pit(void) {
-        io_out8(PIT_PORT_CONTROL, PIT_ICW);
-        io_out8(PIT_PORT_COUNTER0, PIT_COUNTER_VALUE_LOW);
-        io_out8(PIT_PORT_COUNTER0, PIT_COUNTER_VALUE_HIGH);
-
-        /* PITはタイマーでIRQ0なのでPICのIRQ0を解除 */
-        /* io_out8(PIC0_IMR_DATA_PORT, io_in8(PIC0_IMR_DATA_PORT) & (~PIC_IMR_MASK_IRQ0)); */
-    }
+    /* PITはタイマーでIRQ0なのでPICのIRQ0を解除 */
+    /* io_out8(PIC0_IMR_DATA_PORT, io_in8(PIC0_IMR_DATA_PORT) & (~PIC_IMR_MASK_IRQ0)); */
+}
