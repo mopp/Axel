e9cfa8324419e3afee1d0b21f00172128bf9397d
mopp <ac.kxp165@gmail.com>
2014-03-18 20:11:53 +0900

    wip

---
+++ src/include/stdio.h
@@ -0,0 +1,13 @@
+/****************************************************************************************
+ * @file stdio.c
+ * @brief standard io functions in stl of C.
+ * @author mopp
+ * @version 0.1
+ * @date 2014-03-17
+ ****************************************************************************************/
+
+#include <graphic.h>
+#include <stdio.h>
+
+void printf(const char* format, ...) {
+}
---
+++ src/stdio.c
@@ -0,0 +1,17 @@
+/****************************************************************************************
+ * @file include/stdio.h
+ * @brief standard io functions in stl of C.
+ * @author mopp
+ * @version 0.1
+ * @date 2014-03-17
+ ****************************************************************************************/
+
+
+#ifndef STDIO_H
+#define STDIO_H
+
+extern void printf(const char*, ...);
+
+
+
+#endif
