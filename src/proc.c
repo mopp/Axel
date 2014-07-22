369d3f1ae323f26ccacb96bb1e9ac92687931f5e
mopp <ac.kxp165@gmail.com>
2014-07-22 22:51:34 +0900

    make switching function static using function pointer

--- src/proc.c
+++ src/proc.c
@@ -39,16 +39,14 @@ struct task_state_segment {
     uint16_t iomap_base_addr;
 };
 typedef struct task_state_segment Task_state_segment;
-typedef struct task_state_segment Context;
 _Static_assert(sizeof(Task_state_segment) == 104, "Task_state_segment size is NOT 104 byte");
 
-struct segment_selector {
-    uint16_t request_privilege_level : 2;
-    uint16_t table_indicator : 1;
-    uint16_t index : 13;
+struct context {
+    uint32_t sp; /* stack pointer. */
+    uint32_t ip; /* instruction pointer. */
 };
-typedef struct segment_selector Segment_selector;
-_Static_assert(sizeof(Segment_selector) == 2, "Segment_selector size is NOT 16bit");
+typedef struct context Context;
+
 
 struct process {
     uint8_t priority;
@@ -63,7 +61,6 @@ typedef struct process Process;
 
 
 static Process pa, pb, pk;
-/* static Process* current; */
 static char const* name_a = "Process A";
 static char const* name_b = "Process B";
 static char const* name_k = "Process Kernel";
@@ -71,18 +68,19 @@ static Process** processes;
 static uint8_t current_p_idx = 0;
 static uint8_t next_p_idx = 1;
 
-
-#define OFFSETOF(type, field)    ((uintptr_t) &(((type *) 0)->field))
-
-
-void dummy(void) {
-    return;
-}
-
+/* these variables are used in intel syntax inline assembly. */
 static volatile uint32_t* current_ip;
 static volatile uint32_t* current_sp;
 static volatile uint32_t* next_ip;
 static volatile uint32_t* next_sp;
+static void (*exec_switch)(void);
+
+
+
+static void dummy(void) {
+    return;
+}
+
 
 /* TODO: chenge page global directly */
 void switch_context(void) {
@@ -100,10 +98,10 @@ void switch_context(void) {
         next_p_idx = 0;
     }
 
-    current_ip =&current_c->eip;
-    current_sp =&current_c->esp;
-    next_ip = &next_c->eip;
-    next_sp = &next_c->esp;
+    current_ip = &current_c->ip;
+    current_sp = &current_c->sp;
+    next_ip    = &next_c->ip;
+    next_sp    = &next_c->sp;
 
     __asm__ volatile (
         // "pushfl                             \n\t"   // store eflags
@@ -117,7 +115,7 @@ void switch_context(void) {
         // "movl  %%esp, %[current_sp]         \n\t"   // store sp
         // "movl  %[next_sp], %%esp            \n\t"   // restore sp
         // "pushl %[next_ip]                   \n\t"   // restore ip
-        // "jmp dummy                          \n\t"   // change context
+        // "jmp [exec_switch]                          \n\t"   // change context
 
         // "next_turn:                         \n\t"
 
@@ -128,37 +126,40 @@ void switch_context(void) {
         // "popl %%ebp                         \n\t"
         // "popfl                              \n\t"   // restore eflags
 
-        // :   [current_ip] "=m" (current_c->eip),
-        //     [current_sp] "=m" (current_c->esp)
-        // :   [next_ip] "m" (next_c->eip),
-        //     [next_sp] "m" (next_c->esp)
+        // :   [current_ip] "=m" (current_c->ip),
+        //     [current_sp] "=m" (current_c->sp)
+        // :   [next_ip] "m" (next_c->ip),
+        //     [next_sp] "m" (next_c->sp)
         // : "memory"
 
         ".intel_syntax noprefix \n\t"
+        "cli                    \n\t"
         "pushfd                 \n\t"   // store eflags
-        "push ebp               \n\t"
+        "push ebp               \n\t"   // store registers
         "push eax               \n\t"
         "push ecx               \n\t"
         "push edx               \n\t"
         "push ebx               \n\t"
 
-        "lea eax, next_turn     \n\t"   // store ip
-        "mov ebx, [current_ip]  \n\t"   // store ip
-        "mov [ebx], eax         \n\t"   // store ip
+        "lea eax, next_turn     \n\t"   // store ip for next turn.
+        "mov ebx, [current_ip]  \n\t"
+        "mov [ebx], eax         \n\t"
 
-        "mov ebx, [current_sp]  \n\t"   // store ip
-        "mov [ebx], esp         \n\t"   // store sp
+        "mov ebx, [current_sp]  \n\t"   // store sp
+        "mov [ebx], esp         \n\t"
 
         "mov ebx, [next_sp]     \n\t"   // restore sp
-        "mov esp, [ebx]         \n\t"   // restore sp
-        "mov ebx, [next_ip]     \n\t"   // restore sp
-        "mov ebx, [ebx]         \n\t"   // restore sp
-        "push ebx               \n\t"   // restore ip
+        "mov esp, [ebx]         \n\t"
 
-        "jmp dummy              \n\t"   // change context
+        "mov ebx, [next_ip]     \n\t"   // restore ip
+        "mov ebx, [ebx]         \n\t"
+        "push ebx               \n\t"
+
+        "sti                    \n\t"
+        "jmp dummy              \n\t"
 
         "next_turn:             \n\t"
-        "pop ebx                \n\t"
+        "pop ebx                \n\t"   // restore registers
         "pop edx                \n\t"
         "pop ecx                \n\t"
         "pop eax                \n\t"
@@ -190,6 +191,8 @@ void task_b(void) {
 
 
 Axel_state_code init_process(void) {
+    exec_switch = dummy;
+
     pk.context      = vmalloc(sizeof(Context));
     memset(pk.context, 0, sizeof(Context));
     pk.pid          = 0;
@@ -203,19 +206,19 @@ Axel_state_code init_process(void) {
 
     pa.context      = vmalloc(sizeof(Context));
     memset(pa.context, 0, sizeof(Context));
-    pa.context->eip = (uint32_t)(uintptr_t)task_a;
+    pa.context->ip = (uint32_t)(uintptr_t)task_a;
     pa.pid          = 1;
     pa.name         = name_a;
     pa.stack        = vmalloc(0x1000);
-    pa.context->esp = (uint32_t)(uintptr_t)pa.stack;
+    pa.context->sp = (uint32_t)(uintptr_t)pa.stack;
 
     pb.context      = vmalloc(sizeof(Context));
     memset(pb.context, 0, sizeof(Context));
-    pb.context->eip = (uint32_t)(uintptr_t)task_b;
+    pb.context->ip = (uint32_t)(uintptr_t)task_b;
     pb.pid          = 2;
     pb.name         = name_b;
     pb.stack        = vmalloc(0x1000);
-    pb.context->esp = (uint32_t)(uintptr_t)pb.stack;
+    pb.context->sp = (uint32_t)(uintptr_t)pb.stack;
 
     processes = vmalloc(sizeof(Process*) * 3);
     processes[0] = &pk;
