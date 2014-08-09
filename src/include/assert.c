ad60e90a41f9811107606dfbf25658a9e42cfd37
mopp <ac.kxp165@gmail.com>
2014-08-09 10:22:22 +0900

    add dummy assert

---
+++ src/include/assert.c
@@ -0,0 +1,29 @@
+/**
+ * @file assert.c
+ * @brief assertion for debug.
+ * @author mopp
+ * @version 0.1
+ * @date 2014-08-09
+ */
+#ifdef _ASSERT_H_
+
+
+
+#ifdef NDEBUG
+
+
+#define assert(expr)
+
+
+#else
+
+
+/* TODO: make panic function . */
+#define assert(expr)
+
+
+#endif
+
+
+
+#endif
