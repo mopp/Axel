8327a241fbd0b1fef2bd901660f95446e2635389
mopp <ac.kxp165@gmail.com>
2014-10-21 00:14:34 +0900

    [FIX] comment

--- src/kernel.c
+++ src/kernel.c
@@ -68,21 +68,28 @@ enum Gate_descriptor_constants {
 };
 
 
-/* Programmable Interval Timer */
+/*
+ * Programmable Interval Timer
+ * PIT Clock is 1193181.666...Hz
+ * When counter value in PIT, IC fire interruption to IRQ0.
+ * Interrupt frequency = PIT Clock frequency / Counter value.
+ * "Counter value" = "PIT Clock frequency" / "Interrupt frequency".
+ * "Interrupt frequency" is 100 Hz.
+ * Then, "Counter value" = 1193182 / 100 = 11932(0x2E9C)
+ * 1193182 / 0x2E9C(11932) = 100 Hz
+ * 1 / frequency = sec
+ * 1 / 100 = 10 ms
+ * 1 sec / 10 ms = 100 interruption.
+ */
 enum PIT_constants {
-    PIT_PORT_COUNTER0 = 0x40,
-    PIT_PORT_COUNTER1 = 0x41,
-    PIT_PORT_COUNTER2 = 0x42,
-    PIT_PORT_CONTROL = 0x43,
-
-    /* 制御コマンド */
-    /* パルス生成モード */
-    PIT_ICW = 0x34,
-
-    /* カウンター値 */
-    /* 1193182 / 100 Hz */
+    PIT_PORT_COUNTER0      = 0x40,
+    PIT_PORT_COUNTER1      = 0x41,
+    PIT_PORT_COUNTER2      = 0x42,
+    PIT_PORT_CONTROL       = 0x43,
+    PIT_ICW                = 0x34, /* Control word. */
+    PIT_TIMER_1SEC_COUNT   = 100,
     PIT_COUNTER_VALUE_HIGH = 0x2E,
-    PIT_COUNTER_VALUE_LOW = 0x9C,
+    PIT_COUNTER_VALUE_LOW  = 0x9C,
 };
 
 
@@ -139,10 +146,10 @@ _Noreturn void kernel_entry(Multiboot_info* const boot_info) {
         puts("Mouse initialize failed\n");
     }
 
+    io_sti();
     if (AXEL_SUCCESS != init_ide()) {
         printf("Init IDE is failed\n");
     }
-    io_sti();
 
     Window* mouse_win = get_mouse_window();
     Point2d mouse_p = axel_s.mouse->pos = mouse_win->pos;
