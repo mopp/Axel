7afc3eadf5f9180d21ee7cc470872cc1a37bc363
mopp <ac.kxp165@gmail.com>
2014-12-19 17:42:17 +0900

    [FIX] turn on proc

--- src/kernel.c
+++ src/kernel.c
@@ -90,7 +90,7 @@ _Noreturn void kernel_entry(Multiboot_info* const boot_info) {
         printf("Init FileSystem is failed\n");
     }
 
-    /* init_process(); */
+    init_process();
 
     Window* mouse_win = get_mouse_window();
     Point2d mouse_p = axel_s.mouse->pos = mouse_win->pos;
