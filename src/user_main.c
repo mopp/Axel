acf3ae80461f656448612c21b257ec7046123a57
mopp <ac.kxp165@gmail.com>
2015-04-25 18:18:34 +0900

    [FIX] user main

--- src/user_main.c
+++ src/user_main.c
@@ -1,12 +1,50 @@
+#include <syscall_numbers.h>
+#include <stddef.h>
+#include <macros.h>
+#include <aapi.h>
+#include <point.h>
+
+
+static int axel_api_alloc_window(Point2d const*, Point2d const*);
+
 int main(void)
 {
-    __asm__ volatile (
-            "movl $0, %eax \n"
-            "int $0x80  \n"
-            );
-    for(;;) {
-        __asm__ volatile ("movl $16, %ecx");
+    Point2d pos;
+    Point2d size;
+    pos.x  = 500;
+    pos.y  = 500;
+    size.x = 100;
+    size.y = 100;
+    axel_api_alloc_window(&pos, &size);
+
+    for (;;) {
+        __asm__ volatile("movl $16, %ecx");
     }
 
     return 0;
+}
+
+
+#define AXEL_API_TEMPLATE(return_code_var, api_number) \
+    __asm__ volatile(                                  \
+        ".intel_syntax noprefix           \n"          \
+        "mov eax, ebp                     \n"          \
+        "add eax, 8                       \n"          \
+        "push eax                         \n"          \
+        "mov eax,  " TO_STR(api_number)  "\n"          \
+        "push eax                         \n"          \
+        "mov eax, " TO_STR(SYSCALL_AXEL) "\n"          \
+        "int 0x80                         \n"          \
+        "add esp, 8                       \n"          \
+        : "=a"(return_code_var)                        \
+        :                                              \
+        : "eax")
+
+static int axel_api_alloc_window(Point2d const* pos, Point2d const* size)
+{
+    int return_code = 0;
+
+    AXEL_API_TEMPLATE(return_code, AAPI_ALLOC_WINDOW);
+
+    return return_code;
 }
