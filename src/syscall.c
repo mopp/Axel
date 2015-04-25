387849a6e9c43e9bde894f1b5ff09743c5b77779
mopp <ac.kxp165@gmail.com>
2015-04-25 14:25:07 +0900

    [COSM] add comment and fix style

--- src/syscall.c
+++ src/syscall.c
@@ -23,14 +23,15 @@ enum {
 /* Arguments order is Left to Right. */
 union syscall_args {
     size_t args[SYSCALL_MAX_ARG_NUM];
+
     struct axel_args {
-        char* str;
+        size_t api_number;
     } axel_args;
 
     struct execve_args {
-        char const *path;
-        char const * const *argv;
-        char const * const *envp;
+        char const* path;
+        char const* const* argv;
+        char const* const* envp;
     } execve_args;
 
     struct fork_args {
@@ -43,25 +44,24 @@ typedef union syscall_args Syscall_args;
 _Static_assert(sizeof(Syscall_args) == 32, "Syscall_args size is NOT 32 byte.");
 
 
+/* System call handler prototypes for Syscall_entry. */
+static int sys_axel(Syscall_args*);
+static int sys_execve(Syscall_args*);
+static int sys_fork(Syscall_args*);
+
 typedef int (*Syscall_handler)(Syscall_args*);
 
-
 struct syscall_entry {
-    size_t args_byte_size;
-    Syscall_handler func;
+    size_t args_byte_size;   /* Byte size of arguments */
+    Syscall_handler handler; /* Pointer to system call handler function */
 };
 typedef struct syscall_entry Syscall_entry;
 
 
-#define set_syscall_entry(num, name) \
-    [num] = {\
-        sizeof(struct name##_args), sys_##name,\
-    } \
-
-
-static int sys_axel(Syscall_args*);
-static int sys_execve(Syscall_args*);
-static int sys_fork(Syscall_args*);
+#define set_syscall_entry(num, name)            \
+    [num] = {                                   \
+        sizeof(struct name##_args), sys_##name, \
+    }
 
 
 static Syscall_entry syscall_table[] = {
@@ -71,32 +71,40 @@ static Syscall_entry syscall_table[] = {
 };
 
 
-void syscall_enter(Interrupt_frame* iframe) {
-    Syscall_args sa;
-    uint32_t code = iframe->eax;
+/**
+ * @brief entry function of all system call
+ * @param iframe this is used for obtaining system call number and process stack.
+ */
+void syscall_enter(Interrupt_frame* iframe)
+{
+    /* Get system call entry. */
+    uint32_t const syscall_number = iframe->eax;
+    Syscall_entry const* const sentry = &syscall_table[syscall_number];
 
-    Syscall_entry* se = &syscall_table[code];
-
-    /* Get process stack */
-    void* pstack = (void*)iframe->prev_esp;
+    /* Get process stack for arguments */
+    void* process_stack = (void*)iframe->prev_esp;
 
     /* Copy arguments */
-    memcpy(&sa, pstack, se->args_byte_size);
+    Syscall_args sa;
+    memcpy(&sa, process_stack, sentry->args_byte_size);
 
     /* Do system call. */
-    int error = se->func(&sa);
+    int error = sentry->handler(&sa);
 
+    /* Set return value. */
     memcpy(&iframe->eax, &error, sizeof(int));
 }
 
 
-static int sys_axel(Syscall_args* a) {
-    (void)a;
+static int sys_axel(Syscall_args* a)
+{
+    Axel_args* args = (Axel_args*)a;
     return 1;
 }
 
 
-static int sys_execve(Syscall_args* a) {
+static int sys_execve(Syscall_args* a)
+{
     Execve_args* args = (Execve_args*)a;
 
     Axel_state_code r = execve(args->path, args->argv, args->envp);
@@ -105,7 +113,8 @@ static int sys_execve(Syscall_args* a) {
 }
 
 
-static int sys_fork(Syscall_args* a) {
+static int sys_fork(Syscall_args* a)
+{
     (void)a;
     return fork();
 }
