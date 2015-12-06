eb4ea2fd61ab38183bb37f579f9c0b8b414cdff0
mopp <ac.kxp165@gmail.com>
2015-12-06 21:47:32 +0900

    [ADD] clear_screen function

--- src/graphic.rs
+++ src/graphic.rs
@@ -1,14 +1,38 @@
 //! This crate contains stuffs about computer graphic.
 //!
 //! This includes display (text and visual) object.
-#![feature(no_std)]
-#![no_std]
-
 use core::slice;
 
 /// This struct represents any position of 2d-coordinate.
 #[derive(PartialEq, Eq, Debug)]
 pub struct Position(pub isize, pub isize);
+
+
+impl Position {
+    fn x(&self) -> isize {
+        let &Position(x, _) = self;
+        x
+    }
+
+    fn y(&self) -> isize {
+        let &Position(_, y) = self;
+        y
+    }
+}
+
+
+trait Area {
+    fn area_from_origin(&self) -> usize;
+}
+
+
+impl Area for Position {
+    fn area_from_origin(&self) -> usize
+    {
+        (self.x().abs() * self.y().abs()) as usize
+    }
+}
+
 
 #[warn(dead_code)]
 pub enum Color {
@@ -40,13 +64,14 @@ pub trait Display {
     fn set_color_background(&mut self, Color) -> &mut Self;
     fn color_foreground(&self) -> &Color;
     fn set_color_foreground(&mut self, Color) -> &mut Self;
+    fn clear_screen(&mut self);
 }
 
 
 /// Text display struct to represent text display connected to the computer.
 pub struct CharacterDisplay<'a> {
     vram_addr: usize,
-    vram: &'a [u8],
+    vram: &'a mut  [u16],
     current_position: Position,
     max_position: Position,
     color_background: Color,
@@ -59,7 +84,7 @@ impl<'a> Default for CharacterDisplay<'a> {
     {
         CharacterDisplay {
             vram_addr: 0,
-            vram: &[],
+            vram: &mut [],
             current_position: Position(0, 0),
             max_position: Position(0, 0),
             color_background: Color::Black,
@@ -70,17 +95,16 @@ impl<'a> Default for CharacterDisplay<'a> {
 
 
 impl<'a> CharacterDisplay<'a> {
-    pub fn new(vram_addr: usize, maxp: Position) -> CharacterDisplay<'a>
+    pub fn new(vram_addr: usize, max_p: Position) -> CharacterDisplay<'a>
     {
-        let Position(x, y) = maxp;
-        let max_pixels_num = (x * y) as usize;
-        let vram_ptr       = vram_addr as *mut u8;
-        let vram           = unsafe { slice::from_raw_parts_mut(vram_ptr, max_pixels_num) };
+        let max_texts_num  = max_p.area_from_origin();
+        let vram_ptr       = vram_addr as *mut u16;
+        let vram           = unsafe { slice::from_raw_parts_mut(vram_ptr, max_texts_num) };
 
         CharacterDisplay {
             vram_addr: vram_addr,
             vram: vram,
-            max_position: maxp,
+            max_position: max_p,
             ..Default::default()
         }
     }
@@ -91,7 +115,7 @@ impl<'a> CharacterDisplay<'a> {
         self
     }
 
-    fn color(c: &Color) -> i8 {
+    fn color(c: &Color) -> u8 {
         match *c {
             Color::Black        => 0x0,
             Color::Blue         => 0x1,
@@ -113,9 +137,27 @@ impl<'a> CharacterDisplay<'a> {
         }
     }
 
+    /// Generate one pixel (16bit)
+    /// [Text UI](http://wiki.osdev.org/Text_UI)
+    /// Bit 76543210 76543210
+    ///     |||||||| ||||||||
+    ///     |||||||| ^^^^^^^^- Character
+    ///     ||||^^^^-fore colour
+    ///     ^^^^-----back colour
+    pub fn gen_pixel(&self, c:char) -> u16
+    {
+        let bg = Self::color(&self.color_background) as u16;
+        let fg = Self::color(&self.color_foreground) as u16;
+
+        (bg << 12) | (fg << 8) | (c as u16)
+    }
+
     pub fn clear_screen(&mut self)
     {
-        let Position(x, y) = self.max_position;
+        let space = self.gen_pixel(' ');
+        for i in 0..(self.max_position.area_from_origin()) {
+            self.vram[i] = space;
+        }
     }
 }
 
@@ -151,10 +193,22 @@ struct GraphicalDisplay {
 }
 */
 
+#[cfg(test)]
+extern crate core;
 
 #[cfg(test)]
 mod test {
-    use super::{CharacterDisplay, Display, Color, Position};
+    use super::{CharacterDisplay, Display, Color, Position, Area};
+
+    #[test]
+    fn creating_position() {
+        let p = Position(100, 100);
+        assert_eq!(p, Position(100, 100));
+        assert_eq!(p.x(), 100);
+        assert_eq!(p.y(), 100);
+        assert_eq!(p, Position(100, 100));
+        assert_eq!(p.area_from_origin(), 100 * 100);
+    }
 
     #[test]
     fn creating_character_display() {
--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,9 @@ use multiboot::*;
 
 mod graphic;
 
+const TEXT_MODE_VRAM_ADDR: usize = 0xB8000;
+const TEXT_MODE_WIDTH: isize     = 80;
+const TEXT_MODE_HEIGHT: isize    = 25;
 
 #[no_mangle]
 #[start]
@@ -22,15 +25,8 @@ pub extern fn main(multiboot_info_addr: PAddr)
     unsafe {
         mboot = Multiboot::new(multiboot_info_addr, paddr_to_slice);
     }
-    // let display  =  graphic::CharacterDisplay::new(0xB8000, graphic::Position(800, 640));
-
-    let mut i = 0xB8000;
-    while i < 0xC0000 {
-        unsafe {
-            *(i as *mut u16) = 0x1220;
-        }
-        i += 2;
-    }
+    let mut display = graphic::CharacterDisplay::new(TEXT_MODE_VRAM_ADDR, graphic::Position(TEXT_MODE_WIDTH, TEXT_MODE_HEIGHT));
+    display.clear_screen();
 
     loop {
         unsafe {
@@ -50,11 +46,11 @@ pub unsafe fn paddr_to_slice<'a>(p: PAddr, sz: usize) -> Option<&'a [u8]>
 
 
 #[lang = "stack_exhausted"]
-extern fn stack_exhausted() {}
+pub fn stack_exhausted() {}
 
 
 #[lang = "eh_personality"]
-extern fn eh_personality() {}
+pub fn eh_personality() {}
 
 
 #[lang = "panic_fmt"]
