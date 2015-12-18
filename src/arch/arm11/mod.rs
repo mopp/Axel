961fa55d635e965b121ae8902feacf7b20df6117
mopp <ac.kxp165@gmail.com>
2015-12-18 17:38:08 +0900

    [ADD] append module comments

--- src/arch/arm11/mod.rs
+++ src/arch/arm11/mod.rs
@@ -1,3 +1,11 @@
+//! Currently, these module for ARM architecture has been developed for Raspberry Pi Zero.
+//! This Pi's CPU is BCM2835.
+//! References:
+//! (RASPBERRY PI HARDWARE)[https://www.raspberrypi.org/documentation/hardware/raspberrypi/README.md]
+//! (RPi Hub)[http://elinux.org/RPi_Hub]
+//! (RPi BCM2835 GPIOs)[http://elinux.org/RPi_BCM2835_GPIOs]
+//! (BCM2835 datasheet errata)[http://elinux.org/BCM2835_datasheet_errata]
+
 mod peripheral;
 
 pub fn init()
