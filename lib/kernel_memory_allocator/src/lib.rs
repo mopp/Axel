9243834d650f919125a2860add0d6edf5fe15f73
mopp <ac.kxp165@gmail.com>
2016-11-29 15:40:24 +0900

    suppress warnings in the kernel memory allocator for a while because these are annoying

--- lib/kernel_memory_allocator/src/lib.rs
+++ lib/kernel_memory_allocator/src/lib.rs
@@ -1,43 +1,41 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
 #![feature(allocator)]
 #![allocator]
 #![no_std]
 
 
 #[no_mangle]
-pub extern fn __rust_allocate(size: usize, _align: usize) -> *mut u8
+pub extern fn __rust_allocate(size: usize, alignment: usize) -> *mut u8
 {
     panic!("__rust_allocate");
-    unsafe { 0 as *mut u8 }
 }
 
 
 #[no_mangle]
-pub extern fn __rust_deallocate(ptr: *mut u8, _old_size: usize, _align: usize)
+pub extern fn __rust_deallocate(ptr: *mut u8, old_size: usize, alignment: usize)
 {
     panic!("__rust_deallocate");
-    unsafe {}
 }
 
 
 #[no_mangle]
-pub extern fn __rust_reallocate(ptr: *mut u8, _old_size: usize, size: usize, _align: usize) -> *mut u8
+pub extern fn __rust_reallocate(ptr: *mut u8, old_size: usize, size: usize, alignment: usize) -> *mut u8
 {
     panic!("__rust_reallocate");
-    unsafe { 0 as *mut u8 }
 }
 
 
 #[no_mangle]
-pub extern fn __rust_reallocate_inplace(_ptr: *mut u8, old_size: usize, _size: usize, _align: usize) -> usize
+pub extern fn __rust_reallocate_inplace(_ptr: *mut u8, old_size: usize, size: usize, alignment: usize) -> usize
 {
     panic!("__rust_reallocate_inplace");
-    0
 }
 
 
 #[no_mangle]
-pub extern fn __rust_usable_size(size: usize, _align: usize) -> usize
+pub extern fn __rust_usable_size(size: usize, alignment: usize) -> usize
 {
     panic!("__rust_usable_size");
-    size
 }
