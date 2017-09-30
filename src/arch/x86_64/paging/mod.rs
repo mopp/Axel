e945e5308d669c9c6f72971aa7fd8f6b5d48a7dd
mopp <hello@mopp.jp>
2017-09-30 17:21:27 +0900

    feat: add util functions for paging index and its offset

--- src/arch/x86_64/paging/mod.rs
+++ src/arch/x86_64/paging/mod.rs
@@ -35,3 +35,56 @@ mod table;
 
 pub type PhysicalAddress = usize;
 pub type VirtualAddress = usize;
+
+
+trait PageIndex {
+    fn level4_index(self) -> usize;
+    fn level3_index(self) -> usize;
+    fn level2_index(self) -> usize;
+    fn level1_index(self) -> usize;
+    fn offset(self) -> usize;
+}
+
+impl PageIndex for VirtualAddress {
+    fn level4_index(self) -> usize
+    {
+        (self >> 39) & 0o777
+    }
+
+    fn level3_index(self) -> usize
+    {
+        (self >> 30) & 0o777
+    }
+
+    fn level2_index(self) -> usize
+    {
+        (self >> 21) & 0o777
+    }
+
+    fn level1_index(self) -> usize
+    {
+        (self >> 12) & 0o777
+    }
+
+    fn offset(self) -> usize
+    {
+        self & 0xFFF
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_index_functions() {
+        let addr: VirtualAddress = 0o123_456_712_345_1234;
+
+        assert_eq!(addr.offset(), 0o1234);
+        assert_eq!(addr.level1_index(), 0o345);
+        assert_eq!(addr.level2_index(), 0o712);
+        assert_eq!(addr.level3_index(), 0o456);
+        assert_eq!(addr.level4_index(), 0o123);
+    }
+}
