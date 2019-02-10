58ae814213b5e2e41472f818c2d36f7c205e5a62
mopp <hello@mopp.jp>
2019-02-11 02:24:40 +0900

    fix: ErrorCode bits

--- src/arch/x86_64/interrupt/handler.rs
+++ src/arch/x86_64/interrupt/handler.rs
@@ -7,9 +7,9 @@ bitfield! {
     impl Debug;
     external_event, _: 0;
     descriptor_location, _: 1;
-    ti, _: 3, 2;
-    segment_selector_index, _: 3, 15;
-    reserved, _: 16, 31;
+    ti, _: 2;
+    segment_selector_index, _: 13, 3;
+    reserved, _: 31, 14;
 }
 
 pub type Handler = extern "x86-interrupt" fn(error: ErrorCode);
