74e8e447380526c832add65041094fe7ae4b4481
mopp <hello@mopp.jp>
2019-02-17 14:20:36 +0900

    fix: ModeCommandRegister use wrong bitfield

--- src/arch/x86_64/pit.rs
+++ src/arch/x86_64/pit.rs
@@ -1,22 +1,23 @@
 use bitfield::bitfield;
 use lazy_static::lazy_static;
+use spin::Mutex;
 use x86_64::instructions::port::Port;
 
 lazy_static! {
     static ref CHANNEL0: Port<u8> = Port::new(0x40);
-    static ref MODE_COMMAND_REGISTER: Port<u8> = Port::new(0x43);
+    static ref MODE_COMMAND_REGISTER: Mutex<Port<u8>> = Mutex::new(Port::new(0x43));
 }
 
 bitfield! {
     #[repr(C)]
-    pub struct ModeRegister(u8);
+    pub struct ModeCommandRegister(u8);
     impl Debug;
     // 0 refers to four-digit BCD.
     // 1 refers to 16-bit binary mode.
-    bcd_binary_mode, _: 1, 0;
-    operating_mode, _: 3, 1;
-    access_mode, _: 2, 4;
-    select_channel, _: 2, 6;
+    bcd_binary_mode, set_bcd_binary_mode: 1, 0;
+    operating_mode, set_operating_mode: 3, 1;
+    access_mode, set_access_mode: 5, 4;
+    select_channel, set_select_channnel: 7, 6;
 }
 
 pub fn init() {
